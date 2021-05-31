843ce8bcc9978439c64e03e4e4c920aaef1827a5
Анастасія Чуйко <anastasiia.chuiko@nure.ua>
2021-06-01 00:35:32 +0300

    also shit

---
+++ Some/Models/DataModels/Provider.cs
@@ -0,0 +1,15 @@
+﻿using CourseProject.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.Models.DataModels
+{
+    [TableName("Provider")]
+    public class Provider : Entity
+    {
+        public string Name { get; set; }
+
+    }
+}
